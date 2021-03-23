d7060abc1f7ce616541d1ba8caf8bd05f3d5183c
[ Sarah Walls - Unknown - Graduate Student - mlynch11 ] <scwalls@asu.edu>
2021-03-22 19:55:13 -0700

    Added launch job tsrs files

---
+++ launch_job_tsrs_Bs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -n 1
+#SBATCH -t 0-4:00
+#SBATCH -A scwalls
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
+
+module load r/4.0.2
+
+myDir=/scratch/scwalls/STRIPEseq_pipelines/GoSTRIPES_sing/STRIPES/tsr_Bs
+
+echo "Launching job"
+
+cd $myDir
+
+./xrunSwf > err
+
+echo "Job complete"

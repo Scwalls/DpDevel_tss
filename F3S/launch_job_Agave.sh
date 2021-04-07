ba84b651d4a2957987d2f53b76270d9a46fdd878
[ Sarah Walls - Unknown - Graduate Student - mlynch11 ] <scwalls@asu.edu>
2021-04-07 10:20:09 -0700

    Added Makefile and launch_job_Agave scripts

---
+++ F3S/launch_job_Agave.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -n 2
+#SBATCH -t 0-12:00
+#SBATCH -A scwalls
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
+
+myDir=/scratch/scwalls/STRIPEseq_pipelines/GoSTRIPES_sing/STRIPES/
+sampleFolder=F3S
+
+module load singularity/3.6.3
+
+source /home/scwalls/genome_analysis/GoSTRIPES/bin/xworkStripes -b /scratch/scwalls/STRIPEseq_pipelines/GoSTRIPES_sing/STRIPES -i /scratch/scwalls/STRIPEseq_pipelines/GoSTRIPES_sing/STRIPES/gostripes.simg
+
+
+echo "Launching job"
+
+cd $myDir/$sampleFolder
+
+$rws make -n
+
+$rws make
+
+echo "Job complete"
